39071626fc20b5635ce789dd42ea0ac2987f4912
Shibashiba0909 <a990175972@gmail.com>
2023-03-09 22:50:49 +0800

    306

--- TQC/304.cpp
+++ TQC/304.cpp
@@ -4,18 +4,18 @@
 int compute(int a[]) //responsible for finding the return value of a[]
 {
 	int i,n=0;
-	for(i=0;i<6;i++) //A total of six times
-	if(a[i]%3==0)    //take multiples of 3
+	for(i=0;i<6;i++) 
+	if(a[i]%3==0)    //take multiples of 3, and == 0
 	{
-		n++;
+		n++;        //n increment
 	}
 	return n;
 }
  
 int main()
 {
-	int n,ab[6],i;
-	for(i=0;i<6;i++)
-	scanf("%d",&ab[i]);
-	printf("%d\n",compute(ab));
+	int n,arr[6],i;
+	for(i=0;i<6;i++) //A total of six times
+	scanf("%d",&arr[i]);
+	printf("%d\n",compute(arr));
 }
