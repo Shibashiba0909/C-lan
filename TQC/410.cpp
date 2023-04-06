7a18ab8767176d88579e58157f2c390968dd2269
Shibashiba0909 <a990175972@gmail.com>
2023-04-06 23:09:00 +0800

    Chap 4

--- TQC/410.cpp
+++ TQC/410.cpp
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <ctype.h> //"is" prefix
 
 int main()
 {
 	int line,line_count=0;
 	scanf("%d",&line);
-	FILE *read=fopen("read.txt","r");
+	FILE *read=fopen("read.txt","r");    //FILE and fopen = Is a pointer type to a file, used to represent an open file stream
 	FILE *write=fopen("write.txt","w");
-	char buf[100];
-	while(fgets(buf,100,read)!=NULL)
+	char buf[100];  //buf = usually means buffer
+	while(fgets(buf,100,read)!=NULL)  //fgets = Read a line of characters from the file area
 	{
 		int i;
 		line_count++;
-		int len=strlen(buf);
-		if(isalpha(buf[0]))
-			buf[0]=toupper(buf[0]);
-		for(i=1;i<len;i++)
+		int len=strlen(buf);   // strlen = string length
+		if(isalpha(buf[0]))    //isalpha = Tests if a character is a letter 
+			buf[0]=toupper(buf[0]); //toupper = Convert characters to uppercase letter
+		for(i=1;i<len;i++)  //len = May indicate the length of the string
 		{
 			if(buf[i-1]==' '&&isalpha(buf[i]))
 			{
@@ -25,11 +25,11 @@ int main()
 			}
 		}
 		printf("%s",buf);
-		fputs(buf,write);
+		fputs(buf,write);  //fputs = write a line of characters to a file
 		if(line==line_count)
 			break;
 	}
-	fclose(read);
+	fclose(read);   //close an open file
 	fclose(write);
 	return 0;
 } 
