b6dcfc70b05709ef1894df8b8ef1caa48e399ba6
Shibashiba0909 <107416206+Shibashiba0909@users.noreply.github.com>
2024-09-10 14:23:06 +0800

    Update 102.cpp

--- TQC/102.cpp
+++ TQC/102.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int sum(int arr[], int n)
+int main()
 {
-	int i, total=0;
-	for(i=0;i<n;i++)
-		total +=arr[i];
-	return total;
+  int juice;
+  float total;
+  scanf("%d",&juice);
+  total=juice*23.34;
+  printf("%.2f",total);
 }
